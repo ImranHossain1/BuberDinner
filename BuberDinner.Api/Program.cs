0671643a0b3c01a2e60bcd297dbee14eebaaf10a
ImranHossain1 <imranhossain1402@gmail.com>
2023-01-06 19:39:04 +0100

    design the archecture

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,18 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    object value = builder.Services
+    .AddApplication()
+    .AddAInfrastructure();
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+{
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
+}
